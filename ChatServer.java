f946f99aaa44bbdb915f48041c4b55f0e521d56b
braga1233 <braga1233@gmail.com>
2023-09-27 10:11:44 -0300

    ChatServer V1

--- ChatServer.java
+++ ChatServer.java
@@ -9,7 +9,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ChatServer {    
+/*
+ * Copyright (c) 2023
+ * Created by Gabriel Oliveira Braga
+ * Updated by 27/09/2023
+ * Project: https://github.com/GabrielOBraga/RFC-Redes2023
+ * Description: Projeto de Curso Bacharelado de Ciências da Computação, Instituto Federal de Goiás - Campus Anápolis
+ * Discipline: Redes de Computadores - 02/2023
+ */
+public class ChatServer {
+
     private ServerSocket serverSocket;
     private List<ClientHandler> clients;
     private Map<String, ClientHandler> loggedInUsers;
@@ -19,7 +28,7 @@ public class ChatServer {
         server.start(5000);
     }
 
-        public void start(int port) {
+    public void start(int port) {
         clients = new ArrayList<>();
         loggedInUsers = new HashMap<>();
 
@@ -40,15 +49,76 @@ public class ChatServer {
         }
     }
 
+    private synchronized void broadcast(String message, ClientHandler sender) {
+        for (ClientHandler client : clients) {
+            if (client != sender) {
+                client.sendMessage(message);
+            }
+        }
+    }
+
+    private synchronized void sendCommandsList(ClientHandler client){
+        client.sendMessage("\n\n______________________________________________________________\n");
+        client.sendMessage("Comandos para se comunicar o BragaZap!\n");
+        client.sendMessage("/logout - caso você queira sair do Zap!");
+        client.sendMessage("/users - para você ver quais dos seus colegas estão online!");
+        client.sendMessage("/changeuser - para você ser um ninja e alterar seu nome de usuário!");
+        client.sendMessage("/help - cuidado, recursividade encontrada! Error: /help\n");
+    }
+
+    private synchronized void sendUserList(ClientHandler client) {
+        StringBuilder userList = new StringBuilder();
+        userList.append("Passarinhos online:");
+        for (ClientHandler loggedInUser : loggedInUsers.values()) {
+            userList.append("\n- ").append(loggedInUser.getUsername());
+        }
+        client.sendMessage(userList.toString());
+    }
+
+    private synchronized void loginUser(String username, ClientHandler client) {
+        loggedInUsers.put(username, client);
+        client.setUsername(username);
+        broadcast(username + " acabou de pousar no chat.", null);
+        sendUserList(client);
+    }
+
+    private synchronized void logoutUser(ClientHandler client) {
+        client.sendMessage("Você deslogou com sucesso, até a proxima!");
+        loggedInUsers.remove(client.getUsername());
+        clients.remove(client);
+        broadcast(client.getUsername() + " caiu do ninho (deslogou).", null);
+        sendUserList(null);
+    }
+
+    private synchronized void changeUser(String newUsername, ClientHandler client) {
+        loggedInUsers.remove(client.getUsername());
+        loggedInUsers.put(newUsername, client);
+        client.setUsername(newUsername);
+        broadcast(client.getUsername() + " é um ninja, acabou de trocar seu nome para " + newUsername, null);
+        sendUserList(client);
+    }
+
     private class ClientHandler extends Thread {
         private Socket socket;
         private BufferedReader reader;
         private PrintWriter writer;
+        private String username;
 
         public ClientHandler(Socket socket) {
             this.socket = socket;
         }
 
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void sendMessage(String message) {
+            writer.println(message);
+        }
 
         @Override
         public void run() {
@@ -64,6 +134,19 @@ public class ChatServer {
 
                 String clientMessage;
                 while ((clientMessage = reader.readLine()) != null) {
+                    if (clientMessage.startsWith("/logout")) {
+                        logoutUser(this);
+                        break;
+                    } else if (clientMessage.startsWith("/users")) {
+                        sendUserList(this);
+                    } else if (clientMessage.startsWith("/help")) {
+                        sendCommandsList(this);
+                    }else if (clientMessage.startsWith("/changeuser ")) {
+                        String newUsername = clientMessage.substring(12);
+                        changeUser(newUsername, this);
+                    } else {
+                        broadcast(username + ": " + clientMessage, this);
+                    }
                 }
 
                 reader.close();
@@ -74,5 +157,4 @@ public class ChatServer {
             }
         }
     }
-
 }
