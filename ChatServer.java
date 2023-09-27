5624f882fbe5e36a8476465eaf9a8c807ca74916
braga1233 <braga1233@gmail.com>
2023-09-27 10:11:21 -0300

    chat server

--- ChatServer.java
+++ ChatServer.java
@@ -1,5 +1,78 @@
-public class ChatServer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChatServer {    
+    private ServerSocket serverSocket;
+    private List<ClientHandler> clients;
+    private Map<String, ClientHandler> loggedInUsers;
+
     public static void main(String[] args) {
-        System.out.println("Teste Chat");
+        ChatServer server = new ChatServer();
+        server.start(5000);
     }
+
+        public void start(int port) {
+        clients = new ArrayList<>();
+        loggedInUsers = new HashMap<>();
+
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Servidor BragaZap iniciado na porta: " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Novo Passarinho: " + clientSocket);
+
+                ClientHandler client = new ClientHandler(clientSocket);
+                clients.add(client);
+                client.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ClientHandler extends Thread {
+        private Socket socket;
+        private BufferedReader reader;
+        private PrintWriter writer;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+        @Override
+        public void run() {
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new PrintWriter(socket.getOutputStream(), true);
+
+                writer.println("Bem Vindo passarinho, ao BragaZap!");
+
+                writer.println("Como deseja ser chamado? ");
+                String username = reader.readLine();
+                loginUser(username, this);
+
+                String clientMessage;
+                while ((clientMessage = reader.readLine()) != null) {
+                }
+
+                reader.close();
+                writer.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
